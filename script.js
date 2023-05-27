5c7a60dd196c6f2e2bf4766a2f70a4bf9d9f403e
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-05-27 09:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+const players = ["John", "Bob", "Alice", "Poppy","mary"];
 
 const person = {
+   id:1,
   name: "John Doe",
   age: 80,
+  degree:"btech",
+  city:"Newyork"
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
